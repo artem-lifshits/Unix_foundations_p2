f70becf756676e7198285caab05ecf0c98275dc7
arlifshi <artem.lifshits@t-systems.com>
2021-03-01 15:38:05 +0300

    commit

--- 05-USS_extras/3.brace_expantion/28.sh
+++ 05-USS_extras/3.brace_expantion/28.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="${HOME}/UNIX_2_Data/logs"
 echo ${DIR}
-mkdir ${DIR}/{srv01,srv02,srv03}
+mkdir ${DIR}/{srv01,srv02,srv03} 2>/dev/null
 for FILE in ${DIR}/*_{srv01,srv02,srv03}.{access,error}.log; do
   NAME="$(basename ${FILE})"
   NDIR="$( echo "${NAME}" | cut -d '_' -f 1)"
